a285590bc16c73822053e5278135c353406f690c
l0vest0rm <88242590@qq.com>
2017-01-01 17:15:18 +0800

    lockfree queue
    lockfree queue,quicker but more cpu usage

--- example/wordcount/main.go
+++ example/wordcount/main.go
@@ -18,10 +18,8 @@ func main() {
 	}()
 
 	builder := gostream.NewTopologyBuilder()
-	builder.SetSpout("randomWords1", NewSpout(), 4, 100)
-	builder.SetSpout("randomWords2", NewSpout(), 4, 100)
-	bolt := builder.SetBolt("wordcount", NewBolt(), 4, 100)
+	builder.SetSpout("randomWords1", NewSpout(), 1)
+	bolt := builder.SetBolt("wordcount", NewBolt(), 1, 100)
 	bolt.KeyGrouping("randomWords1", "word")
-	bolt.KeyGrouping("randomWords2", "word")
 	builder.Run()
 }
